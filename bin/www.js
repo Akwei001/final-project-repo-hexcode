80408555d586957a5656b81fb2daec5c365240f9
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-01-16 17:36:31 +0000

    fallback port commented out

--- bin/www.js
+++ bin/www.js
@@ -3,10 +3,12 @@ import app from "../app.js";
 import { debugOut, infoOut } from "../utils/logging.js";
 
 // if there's a port for a remote deployment on, say, Heroku, use that, otherwise go for local host, which i would set to 3000, or 3001 or 5000 etc.
-const PORT_FALLBACK_VALUE = 5000;
+// const PORT_FALLBACK_VALUE = 5000;
 //changed to 3000 from 5000 ao it works for development on Mac's
 // update - changed back to 5000 so it didnt clash with frontend on localhost as same time
-const PORT = process.env.PORT || PORT_FALLBACK_VALUE;
+const PORT = process.env.PORT;
+
+// PORT_FALLBACK_VALUE;
 
 //remember app = express(), and express has been imported from 'express';
 app.listen(PORT, () => {
