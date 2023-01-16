564f6e6c4972f3382049c433858691d7f1149471
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-01-16 13:41:23 +0000

    config js changes

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // export const DATABASE_SYSTEM_POSTGRESQL = "POSTGRESQL";
 // export const DATABASE_SYSTEM_COUCHBASE = "COUCHBASE";
 
-export const ENV_TYPE_IS_DEVELOPMENT = "DEVELOPMENT"; // do not changed - set up  in .env, and in Heroku  twice (one on staging app; once on prod app)
+// export const ENV_TYPE_IS_DEVELOPMENT = "DEVELOPMENT"; // do not changed - set up  in .env, and in Heroku  twice (one on staging app; once on prod app)
 // export const ENV_TYPE_IS_STAGING = "STAGING"; //do not change - set in Heroku staging App
-// export const ENV_TYPE_IS_PRODUCTION = "PRODUCTION"; //do not change - set in Heroku Live App
+export const ENV_TYPE_IS_PRODUCTION = "PRODUCTION"; //do not change - set in Heroku Live App
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const ENV_TYPE = process.env.ENV_TYPE;
